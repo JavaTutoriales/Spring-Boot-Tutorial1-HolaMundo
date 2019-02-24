1de30fdfaa4d702229ae04d783dc6079e1fb94d4
Programador Java <programadorjavablog@gmail.com>
2019-02-24 12:29:11 -0600

    Se agrega la cabecera de los arhivos, conteniendo la ruta del tutorial en el blog.

--- tutorial1/src/main/java/com/javatutoriales/stringboot/tutorial1/controllers/HelloWorldController.java
+++ tutorial1/src/main/java/com/javatutoriales/stringboot/tutorial1/controllers/HelloWorldController.java
@@ -1,3 +1,14 @@
+/* 
+ * Clase Java desarrollada por Alex para el blog Java Tutoriales (javatutoriales.com) el día 24-feb-2019
+ * 
+ * Para informacion sobre el uso de esta clase, asi como bugs, actualizaciones, o mejoras enviar un mail a
+ * programadorjavablog@gmail.com
+ * 
+ * Para ver el tutorial completo consulta https://www.javatutoriales.com/2019/02/spring-boot-parte-1-introduccion-y-hola.html
+ * 
+ * No olvides hacerte fan de Java Tutoriales en facebook https://www.facebook.com/JavaTutoriales/ y seguirme en Twitter: https://twitter.com/JavaTutoriales
+ * 
+*/
 package com.javatutoriales.stringboot.tutorial1.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
